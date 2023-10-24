26e5f5fd1ef6a68c2fcd2e990a2693b6877f65ce
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-24 15:27:55 -0600

    frontend registro y login terminado

--- views/login.php
+++ views/login.php
@@ -1,12 +1,42 @@
 <?php 
 include 'header.php';
+include 'navbar.php';
 ?>
+
+<style>
+    .card {
+        border-radius: 15px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        transition: transform 0.3s;
+    }
+
+    .card:hover {
+        transform: scale(1.005);
+    }
+
+    .form-label {
+        font-weight: 600;
+    }
+
+    .btn-primary {
+        background-color: #007bff;
+        border-color: #007bff;
+        transition: background-color 0.3s, border-color 0.3s;
+    }
+
+    .btn-primary:hover {
+        background-color: #0056b3;
+        border-color: #0056b3;
+    }
+</style>
 
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">Inicio de sesión</div>
+            <div class="card animate__animated animate__fadeIn">
+                <div class="card-header bg-primary text-white">
+                    <h4 class="mb-0">Inicio de sesión</h4>
+                </div>
                 <div class="card-body">
                     <form action="/controllers/auth/AuthController.php" method="post">
                         <div class="mb-3">
@@ -17,7 +47,7 @@ include 'header.php';
                             <label for="contrasena" class="form-label">Contraseña</label>
                             <input type="password" name="contrasena" class="form-control" id="contrasena" placeholder="Contraseña" required>
                         </div>
-                        <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+                        <button type="submit" class="btn btn-primary btn-block animate__animated animate__pulse animate__infinite">Iniciar sesión</button>
                     </form>
                 </div>
             </div>
